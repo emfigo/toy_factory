69a206ff3351c1ded790ddf56aef0e096629fbcb
James Cook <jamesmarkcook@gmail.com>
2014-04-07 12:03:33 +0100

    Pass PhaseII test

--- lib/toy_factory/dealer.rb
+++ lib/toy_factory/dealer.rb
@@ -1,4 +1,15 @@
 module ToyFactory
   class Dealer
+
+    def initialize(order)
+      @order = order
+    end
+
+    def make_order
+      if @order == 'toy'
+        'toy'
+      end
+    end
+
   end
 end
