7ee631aef6f148bb2c84f9857b6cbbbe5cddf5ab
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-04-07 11:50:39 +0100

    completed phase I & II

--- lib/toy_factory/dealer.rb
+++ lib/toy_factory/dealer.rb
@@ -1,4 +1,12 @@
 module ToyFactory
   class Dealer
+    attr_accessor :order
+    def initialize(order)
+      @order = order
+    end
+
+    def make_order
+      order
+    end
   end
 end
--- lib/toy_factory/factories/factory.rb
+++ lib/toy_factory/factories/factory.rb
@@ -1,6 +1,9 @@
 module ToyFactory
   class Factory
     def self.build(order)
+      if order == "toy"
+        Toy
+      end
     end
   end
 end
