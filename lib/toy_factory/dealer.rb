bc44490312a007017cb9b06a9b1284f1278f9f4e
Rosie Brigham <rosie.brigham@fundingcircle.com>
2014-04-07 12:16:18 +0100

    phase III

--- lib/toy_factory/dealer.rb
+++ lib/toy_factory/dealer.rb
@@ -1,4 +1,19 @@
 module ToyFactory
   class Dealer
+
+    def initialize(order)
+      @order = order
+    end
+
+    def make_order
+      @order
+    end
+
   end
 end
+
+
+# if customer has an order,
+# create a Dealer with an order
+#  and return the specified toy to the customer
+# when the toy is specified, return that toy to the customer
--- lib/toy_factory/factories/factory.rb
+++ lib/toy_factory/factories/factory.rb
@@ -1,6 +1,21 @@
 module ToyFactory
   class Factory
     def self.build(order)
+      if order == 'toy'
+        Toy
+      elsif order == "car"
+        Car
+      elsif order == "doll"
+        Doll
+      elsif order == "robot"
+        Robot
+      elsif order == "puzzle"
+        Puzzle
+      else order == "waterproof_toy"
+        WaterproofToy          
+      end
     end
+
   end
+
 end
